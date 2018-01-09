7ba82d0d4293332500cbe9725603d09f5653b189
sanchezmoreno.mauricio58@gmail.com <Mauri@LAPTOP-C9HGII3C>
2018-01-09 17:08:29 +0100

    añadida la clase principal

---
+++ src/Controler/LibretaDirecciones.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controler;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Mauri
+ */
+public class LibretaDirecciones extends Application {
+    
+    private Stage escenarioPrincipal;
+    private BorderPane layoutPrincipal;
+    private AnchorPane vistaPersona;
+    
+    
+    @Override
+    public void start(Stage escenarioPrincipal) {
+        
+        this.escenarioPrincipal=escenarioPrincipal;
+        //establecemos el titulo
+        this.escenarioPrincipal.setTitle("Libreta de Direcciones");
+        
+        //llamamos a los metodos que tenemos que utilizar
+        //este para los layouts de la clase principal
+        initLayoutPrincipal();
+        //y este para mostrar la vista persona
+        muestraVistaPersona();
+        
+        
+        
+    }
+    
+    //han de ser publicos por si necesitamos llamarlos desde otros controladores
+      public void initLayoutPrincipal() {
+
+          //cargamos el layout principal a partir de la vistaPrincipal.FXML
+          FXMLLoader loader = new FXMLLoader();
+          //la ruta salimos de donde estamos con .., y le pasamos la ruta otra vez
+          URL location = LibretaDirecciones.class.getResource("../VistaPrincipal.fxml");
+          loader.setLocation(location);
+        try {
+            layoutPrincipal = loader.load();
+        } catch (IOException ex) {
+
+        }
+          
+        //cargo y muestro la escena que contiene ese layout principal
+        
+        Scene escena = new Scene(layoutPrincipal);
+        escenarioPrincipal.setScene(escena);
+        escenarioPrincipal.show();
+          
+          
+      }
+
+    public void muestraVistaPersona() {
+
+    }
+    
+
+   
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+  
+}
