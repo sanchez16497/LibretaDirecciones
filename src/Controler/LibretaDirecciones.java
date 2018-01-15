d1d70e73cca64a4aba293b15aa1eeafeb0f85db5
sanchezmoreno.mauricio58@gmail.com <Mauri@LAPTOP-C9HGII3C>
2018-01-15 17:06:13 +0100

    hemos añadido vistaController.java

--- src/Controler/LibretaDirecciones.java
+++ src/Controler/LibretaDirecciones.java
@@ -5,11 +5,15 @@
  */
 package Controler;
 
+import Model.Persona;
+import View.VistaPersonaController;
 import java.io.IOException;
 import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
@@ -26,10 +30,27 @@ import javafx.stage.Stage;
  */
 public class LibretaDirecciones extends Application {
     
+    private ObservableList datosPersona = FXCollections.observableArrayList();
     private Stage escenarioPrincipal;
     private BorderPane layoutPrincipal;
     private AnchorPane vistaPersona;
     
+     public LibretaDirecciones(){
+        
+        datosPersona.add(new Persona("Mauricio", "Sanchez Moreno"));
+        datosPersona.add(new Persona("Juan", "Pérez Martínez"));
+        datosPersona.add(new Persona("Andrea", "Chenier López"));
+        datosPersona.add(new Persona("Emilio", "González Pla"));
+        datosPersona.add(new Persona("Mónica", "de Santos Sánchez"));
+        
+    }
+    
+         //Método para devolver los datos como lista observable de personas
+    public ObservableList getDatosPersona() {
+        return datosPersona;
+    }
+     
+     
     
     @Override
     public void start(Stage escenarioPrincipal) {
@@ -71,21 +92,23 @@ public class LibretaDirecciones extends Application {
 
      public void muestraVistaPersona() {
         //creamos el cargador
-        FXMLLoader cargador = new FXMLLoader();
+        FXMLLoader cargadorVista = new FXMLLoader();
         //damos la url correspondiente a la otra vista, VistaPersonal.fxml
         URL location = LibretaDirecciones.class.getResource("../View/VistaPersona.fxml");
         //cogemos la url y se la pasamos al cargador
-        cargador.setLocation(location);
+        cargadorVista.setLocation(location);
         
         try {
-            vistaPersona=cargador.load();
+            vistaPersona=cargadorVista.load();
         } catch (IOException ex) {
             
         }
         //añadimos la vista al layout central
         layoutPrincipal.setCenter(vistaPersona);
         
-                
+        //Doy acceso al controlador VistaPersonaCOntroller a LibretaDirecciones
+        VistaPersonaController controller = cargadorVista.getController();
+         controller.setLibretaDirecciones(this);       
     }
     
       //Invoco el método getPrimaryStage para que devuelva mi escenario principal
